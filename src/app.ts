1fa5945d120c4291eabc9e7f93a581b55236b078
yong seock <myongdol132@gmail.com>
2023-09-08 23:54:06 +0900

    env파일, api키  설정

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+const GoogleAPI = process.env.GOOGLE_API_KEY;
 
 function 주소검색핸들러(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
-
 
 }
 
