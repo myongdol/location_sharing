3bdde7415188421ccf3a30e77e1f8b607f4e67d6
yong seock <myongdol132@gmail.com>
2023-09-09 00:18:00 +0900

    사용자가 입력한 주소의 좌표값 가져오기

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
+import axios from 'axios';
 require('dotenv').config();
+
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 const GoogleAPI = process.env.GOOGLE_API_KEY;
+
+type GoogleGeoType = {
+    results: { geometry: { location: {lat: number, lng: number} } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
 
 function 주소검색핸들러(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
+    axios.get<GoogleGeoType>(`
+    https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+        )}&key=${GoogleAPI}`
+        )
+        .then(response => {
+            if (response.data.status !== 'OK') {
+                throw new Error('위치정보를 가져올수 없습니다')
+            }
+            const coordinates = response.data.results[0].geometry.location;
+        })
+        .catch(err => {
+            alert(err.message);
+            console.log(err);
+        })
+        ;
 }
 
 form.addEventListener('submit', 주소검색핸들러);
