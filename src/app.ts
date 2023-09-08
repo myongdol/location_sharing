74d55ec28fa472d1d59a4940afdc7f9575236ca0
yong seock <myongdol132@gmail.com>
2023-09-08 23:38:22 +0900

    사용자 입력 핸들링

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function 주소검색핸들러(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+
+}
+
+form.addEventListener('submit', 주소검색핸들러);
